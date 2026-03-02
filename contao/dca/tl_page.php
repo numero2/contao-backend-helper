607da164eee23899aafba7c3096c4adece564992
Benny Born <benny.born@numero2.de>
2026-03-02 09:49:55 +0100

    make sure info fields are copied

--- contao/dca/tl_page.php
+++ contao/dca/tl_page.php
@@ -6,7 +6,7 @@
  * @author    Benny Born <benny.born@numero2.de>
  * @author    Michael Bösherz <michael.boesherz@numero2.de>
  * @license   LGPL-3.0-or-later
- * @copyright Copyright (c) 2025, numero2 - Agentur für digitales Marketing GbR
+ * @copyright Copyright (c) 2026, numero2 - Agentur für digitales Marketing GbR
  */
 
 
@@ -17,6 +17,6 @@ $GLOBALS['TL_DCA']['tl_page']['fields']['bh_info'] = [
     'inputType'             => 'text'
 ,   'label'                 => &$GLOBALS['TL_LANG']['MSC']['bh_info']
 ,   'exclude'               => true
-,   'eval'                  => ['maxlength'=>255, 'doNotCopy'=>true, 'tl_class'=>'w50']
+,   'eval'                  => ['maxlength'=>255, 'tl_class'=>'w50']
 ,   'sql'                   => "varchar(255) NOT NULL default ''"
 ];
